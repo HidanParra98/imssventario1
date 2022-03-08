a0b985169f747fed40fdd85675b4c20ece16bbb8
DESKTOP-R6ASI0J\Hidan <hidan.parra@gmail.com>
2022-03-08 05:44:09 -0500

    Se carga archivo al repo

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img class="logos" alt="Logo del IMSS" src="../assets/imsslogo.png">
+    <img class="logos" alt="Logo del Conalep" src="../assets/logoconalep.png">
+    <HelloWorld msg="Inventario de Enfermeria UMF No.17"/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style>
+  .logos {
+    width: 180px;
+    height: 200px;
+    margin: 1em;
+  }
+</style>
